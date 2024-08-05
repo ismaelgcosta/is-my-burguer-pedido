48fe0c6903f42362d355e8946469e4b12461d8cf
Ismael G Costa <notmymail@notmymail.com>
2024-08-05 20:38:15 -0300

    testes unitários

--- api-pedido/api-pedido-core/api-pedido-use-case/src/test/java/br/com/ismyburguer/pedido/usecase/impl/AlterarStatusPedidoUseCaseImplTest.java
+++ api-pedido/api-pedido-core/api-pedido-use-case/src/test/java/br/com/ismyburguer/pedido/usecase/impl/AlterarStatusPedidoUseCaseImplTest.java
@@ -60,7 +60,7 @@ public class AlterarStatusPedidoUseCaseImplTest {
 
         alterarStatusPedidoUseCase.alterar(new Pedido.PedidoId(pedidoId), novoStatus);
 
-        verify(pedidoMock).alterarStatus(novoStatus);
+        verify(pedidoMock, times(2)).alterarStatus(novoStatus);
         verify(repository).alterar(new Pedido.PedidoId(pedidoId), novoStatus);
         verify(cancelarPagamentoUseCase, never()).cancelar(any());
     }
